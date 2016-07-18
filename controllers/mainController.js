9de71ee967ccc5180d8bd4d2e3b0169f8c5131b5
MehdiHennaoui <medh31@gmail.com>
2016-07-18 03:11:32 +0200

    ajout de la methode getUser qui permet de récupérer le nom de l'utilisateur souncloud -ajout d'un formulaire qui récupére le user -faire le lien entre le mainController et la tracksFactory pour le user -création de l'argument userName et de concaténé le userName dans l'Url de la promesse

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,4 +1,8 @@
 angular.module("app").controller("MainCtrl", function($scope, tracksFactory){
+	
+	$scope.getUser = function(userName){
+		tracksFactory.getTracks(userName);
+	}
 	// récupere la fonction getTracks sur la factory tracksFactory
 	$scope.tracks= console.log(tracksFactory.getTracks());
 
--- services/tracksFactory.js
+++ services/tracksFactory.js
@@ -1,11 +1,11 @@
 angular.module("app").factory("tracksFactory", function($http, $q){
 	// promesse sur l'api souncloud sur le compte de lolivier pour récupere les chansons
-	function getTracks(){
+	function getTracks(userName){
 		var deferred = $q.defer();
 		
 			$http({
 				method: "GET",
-				url: "http://api.soundcloud.com/users/lolivieretcetera/tracks?client_id=bda4ada8694db06efcac9cf97b872b3e",
+				url: "http://api.soundcloud.com/users/" + userName + "/tracks?client_id=bda4ada8694db06efcac9cf97b872b3e",
 				}).then(function(results){
 					console.log("results", results);
 					console.log("results.data", results.data);
